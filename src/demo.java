9481b95e09c6e5af4b885c40557954b5a3f320bc
wzm <1458775494@qq.com>
2020-04-11 15:59:47 +0800

    test05

--- src/demo.java
+++ src/demo.java
@@ -7,6 +7,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("使用git拉取项目");
         System.out.println("使用git拉取项目");
     }
