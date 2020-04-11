b35c6eb9a694b7a7765968500b1a5485aa34e740
wzm <1458775494@qq.com>
2020-04-11 15:29:57 +0800

    test01

---
+++ src/demo.java
@@ -0,0 +1,10 @@
+/**
+ * @Description: TODO
+ * @Author wenzhimin
+ * @Date 2020/4/11
+ **/
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
