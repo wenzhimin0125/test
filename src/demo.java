c01a9e733b8b3f7122a132884df502bbda9ab15f
wzm <1458775494@qq.com>
2020-04-11 18:15:41 +0800

    test11

--- src/demo.java
+++ src/demo.java
@@ -15,5 +15,6 @@ public class demo {
         System.out.println("使用git拉取项目");
         System.out.println("创建分支");
         System.out.println("创建分支2");
+        System.out.println("合并分支");
     }
 }
